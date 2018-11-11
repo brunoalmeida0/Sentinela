6d8db1c50937374d5545693b42900523cdaa42a1
UlissesGomes <ulisses67@hotmail.com>
2018-11-11 00:22:50 -0300

    Retrofit

---
+++ app/src/main/java/com/example/cybersecurity/hackaton/sentinela2/data/RetrofitInit.java
@@ -0,0 +1,23 @@
+package com.example.cybersecurity.hackaton.sentinela2.data;
+
+import com.example.cybersecurity.hackaton.sentinela2.service.ReportService;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+public class RetrofitInit {
+
+    private final Retrofit retrofit;
+
+    public RetrofitInit() {
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl("http://172.29.40.44:8080/")
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+    }
+
+    public ReportService getReportService() {
+        return this.retrofit.create(ReportService.class);
+    }
+
+}
